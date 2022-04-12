839a7e8ca895ccc4c62fb369efec6b19efd54364
LuMi <luimiguz1520@hotmail.com>
2022-04-12 17:04:24 -0500

    ok

---
+++ 01-sumar-contract/suma.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract suma{
+    function sumar(int num1,int num2) public pure returns (int){
+        return num1 + num2;
+    }
+}
