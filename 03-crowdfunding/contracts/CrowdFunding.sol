f16893df3baff00ae0e9e5d3be8da81786176890
LuMi <lumigu.dev@gmail.com>
2022-06-06 10:29:04 -0500

    adding some errors messages to revert the operation

--- 03-crowdfunding/contracts/CrowdFunding.sol
+++ 03-crowdfunding/contracts/CrowdFunding.sol
@@ -7,10 +7,18 @@ contract crowdFunding{
     string public description;
     address payable public author;
     address private owner;
-    string public state = 'Opened';
-    bool fundable;
+    uint public state;
     uint public funds;
     uint public fundraisingGoal;
+
+    event projectFounded(
+        string projectid,
+        uint value
+    );
+    event stateProject(
+        string id,
+        uint state
+    );
 
     constructor(string memory _id, string memory _name, string memory _description, uint _fundraisingGoal){
         id = _id;
@@ -30,24 +38,21 @@ contract crowdFunding{
     modifier notAuthor(){
         require(
             author != msg.sender,
-            "You can't found the project yourselft"
+            "You can't fund the project yourselft"
         );
         _;
     }
-    event projectFounded(
-        string projectid,
-        uint value
-    );
-    event stateProject(
-        string state
-    );
+    
     function fundProject() public payable notAuthor{
+        require(state != 1, "The project is closed can not recive funds");
+        require(msg.value > 0, "Fund value must be greater than 0");
         author.transfer(msg.value);
         funds += msg.value;
         emit projectFounded(id, msg.value);
     }
-    function changeProjectSatate(string calldata newSatate) public onlyAuthor{
-        state = newSatate;
-        emit stateProject(state);
+    function changeProjectSatate(uint newState) public onlyAuthor{
+        require(state != newState, "New state must be different");
+        state = newState;
+        emit stateProject(id, newState);
     }
 }
